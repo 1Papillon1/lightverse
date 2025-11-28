de1d5eb75703e41a41d408cb3e99be3240abeee0
Marijan Kopčić <mkopcic@gmail.com>
2025-11-28 21:34:43 +0100

    Universe Backdrop addition to Auth page.

--- resources/js/pages/Authorization.jsx
+++ resources/js/pages/Authorization.jsx
@@ -4,6 +4,7 @@ import { Head, usePage } from "@inertiajs/react";
 import Signup from "@/components/authentication/Signup";
 import Login from "@/components/authentication/Login";
 import MainLayout from "@/MainLayout";
+import UniverseBackdrop from "@/components/visuals/UniverseBackdrop";
 
 export default function Authorization() {
   const { mode } = usePage().props;  // 'login' ili 'signup'
@@ -12,6 +13,7 @@ export default function Authorization() {
     <>
       <Head title={mode === "login" ? "Login" : "Sign Up"} />
       {mode === "signup" ? <Signup /> : <Login />}
+      <UniverseBackdrop mode={mode} />
     </>
   );
 }
